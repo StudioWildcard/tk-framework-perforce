392083d17be33272d569ae22995e1bf03e9d726e
infintropy <36432437+infintropy@users.noreply.github.com>
2022-08-08 07:40:18 -0400

    refactoring

--- python/widgets/base_model.py
+++ python/widgets/base_model.py
@@ -4,41 +4,22 @@ from .item_schemas import ItemSchema
 
 logger = sgtk.platform.get_logger(__name__)
 
-class SortFilterModel(QtGui.QSortFilterProxyModel):
-    """
-    A proxy model that excludes files from the view
-    that end with the given extension
-    """
-    def __init__(self, excludes, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._excludes = excludes[:]
-
-    def filterAcceptsRow(self, srcRow, srcParent):
-        idx = self.sourceModel().index(srcRow, 0, srcParent)
-        name = idx.data()
-        print("Filtered: {}".format(name))
-        for i in self._excludes:
-            if i in name:
-                return False
-        
-        return True
 
 class MultiModel(QtCore.QAbstractItemModel):
     def __init__(self, data=None, parent=None):
         super().__init__(parent)
 
-        #self.rootItem will specify your headlines
+        # self.rootItem will specify your headlines
         # TODO: pass your tree item in
-        self.rootItem = ItemSchema(schema='asset_item_schema')
-        
+        self.rootItem = ItemSchema(schema="asset_item_schema")
+
         # TODO: make this work with lists of lists of strings
         if data:
             self.setupModelData(data, self.rootItem)
 
-        logger.debug('was able to set up the Model!! {}'.format(data))
+        logger.debug("was able to set up the Model!! {}".format(data))
 
         self.primary_roots = {}
-
 
     def columnCount(self, parent=None):
         if not parent:
@@ -70,7 +51,11 @@ class MultiModel(QtCore.QAbstractItemModel):
         if not index.isValid():
             return QtCore.Qt.NoItemFlags
 
-        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
+        return (
+            QtCore.Qt.ItemIsEnabled
+            | QtCore.Qt.ItemIsSelectable
+            | QtCore.Qt.ItemIsEditable
+        )
 
     def headerData(self, section, orientation, role):
         if orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole:
@@ -94,7 +79,6 @@ class MultiModel(QtCore.QAbstractItemModel):
             return self.createIndex(row, column, childItem)
         else:
             return QtCore.QModelIndex()
-
 
     def parent(self, index):
         if not index.isValid():
@@ -120,21 +104,28 @@ class MultiModel(QtCore.QAbstractItemModel):
             parentItem = parent.internalPointer()
 
         return parentItem.childCount()
-    
+
     def add_row(self, data_item):
         if data_item.get("asset_name"):
-            if not self.primary_roots.get(data_item['asset_name']):
-                asset_item = ItemSchema(data=data_item, parent=self.rootItem, schema='asset_item_schema', primary=True)
-                self.primary_roots[data_item['asset_name']] = asset_item
-            sync_item = ItemSchema(data=data_item, parent=self.primary_roots[data_item['asset_name']], schema='sync_item_schema') 
+            if not self.primary_roots.get(data_item["asset_name"]):
+                asset_item = ItemSchema(
+                    data=data_item,
+                    parent=self.rootItem,
+                    schema="asset_item_schema",
+                    primary=True,
+                )
+                self.primary_roots[data_item["asset_name"]] = asset_item
+            sync_item = ItemSchema(
+                data=data_item,
+                parent=self.primary_roots[data_item["asset_name"]],
+                schema="sync_item_schema",
+            )
             self.layoutAboutToBeChanged.emit()
             self.dataChanged.emit(QtCore.QModelIndex(), QtCore.QModelIndex())
             self.layoutChanged.emit()
-        #lines is our list
+        # lines is our list
+
     def setupModelData(self, data, parent):
 
         for list_item in data:
             self.add_row(list_item)
-
-            
-            
--- python/widgets/filter_models.py
+++ python/widgets/filter_models.py
@@ -1,21 +1,35 @@
 import sgtk
 from sgtk.platform.qt import QtCore, QtGui
 
+
 class SortFilterModel(QtGui.QSortFilterProxyModel):
     """
     A proxy model that excludes files from the view
     that end with the given extension
     """
+
     def __init__(self, excludes, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._excludes = excludes[:]
 
-    def filterAcceptsRow(self, srcRow, srcParent):
+    def filterAcceptsRow(self, srcRow, srcParent) -> bool:
+        """
+        When asked if a given row is to be filtered or remain,
+        use logic to decide and return true/false for the whole
+        row.
+
+        Args:
+            srcRow (_type_): _description_
+            srcParent (_type_): _description_
+
+        Returns:
+            bool: True if row is to remain, False if to be filtered
+        """
         idx = self.sourceModel().index(srcRow, 0, srcParent)
         name = idx.data()
         print("Filtered: {}".format(name))
         for i in self._excludes:
             if i in name:
                 return False
-        
+
         return True
