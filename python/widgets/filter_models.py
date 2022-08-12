9f5025fe30160089eb868ab27f8314cf7cc5b375
infintropy <36432437+infintropy@users.noreply.github.com>
2022-08-12 19:50:46 -0400

    using extention as filter quality

--- python/widgets/filter_models.py
+++ python/widgets/filter_models.py
@@ -8,9 +8,10 @@ class SortFilterModel(QtGui.QSortFilterProxyModel):
     that end with the given extension
     """
 
-    def __init__(self, excludes, *args, **kwargs):
+    def __init__(self, excludes, *args, parent=None, **kwargs, ):
         super().__init__(*args, **kwargs)
         self._excludes = excludes[:]
+        self.main_app = parent
 
     def filterAcceptsRow(self, srcRow, srcParent) -> bool:
         """
@@ -25,11 +26,16 @@ class SortFilterModel(QtGui.QSortFilterProxyModel):
         Returns:
             bool: True if row is to remain, False if to be filtered
         """
-        idx = self.sourceModel().index(srcRow, 0, srcParent)
+        idx = self.sourceModel().index(srcRow, 1, srcParent)
         name = idx.data()
-        print("Filtered: {}".format(name))
-        for i in self._excludes:
-            if i in name:
+        
+        include = self.main_app.utils.prefs.data.get('ext_filters')
+        include_list = [k for k,v in include.items() if v==True]
+        self.main_app.logger.info(name )
+        if name:
+            if name not in include_list:
+                print("Filtered: {}".format(name))
                 return False
+
 
         return True
