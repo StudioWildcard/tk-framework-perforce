a81d194ead3a85b65e41328f45e0ad07a015d85d
infintropy <36432437+infintropy@users.noreply.github.com>
2022-08-15 18:51:05 -0400

    visibility state handled/tracked by main filter, so that it can be accessed in each item

--- python/widgets/filter_models.py
+++ python/widgets/filter_models.py
@@ -17,7 +17,7 @@ class SortFilterModel(QtGui.QSortFilterProxyModel):
     ):
         super().__init__(*args, **kwargs)
         self._excludes = excludes[:]
-        self.main_app = parent
+        self.main_ui = parent
 
     def filterAcceptsRow(self, srcRow, srcParent) -> bool:
         """
@@ -38,22 +38,27 @@ class SortFilterModel(QtGui.QSortFilterProxyModel):
 
             parent_item = self.sourceModel().item(srcParent)
             item = parent_item.child(srcRow)
+            item.visible = True
 
             if hasattr(item, "primary"):
                 if item.primary:
+
                     return True
             # # else:
             name = item.itemData[2]
-            self.main_app.logger.error(str(item.itemData))
-            include = self.main_app.utils.prefs.data.get("ext_filters")
-            self.main_app.logger.info(include)
+
+            # self.main_app.logger.error(str(item.itemData))
+            include = self.main_ui.utils.prefs.data.get("ext_filters")
+            # self.main_app.logger.info(include)
             include_list = [k for k, v in include.items() if v is True]
             # self.main_app.logger.info(str())
             # # self.main_app.logger.info(tree_item)
+
             if name:
                 if name not in include_list:
-                    self.main_app.logger.error("HIDIN!!!!!: {}".format(name))
+                    # self.main_app.logger.error("HIDIN!!!!!: {}".format(name))
                     print("Filtered: {}".format(name))
+                    item.visible = False
                     return False
             return True
         except Exception as e:
